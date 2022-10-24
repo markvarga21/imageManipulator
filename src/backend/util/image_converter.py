15582f65fec3311a60e13c756495f9986b94af78
Varga József Márk <vmark2145@gmail.com>
2022-10-24 09:44:55 +0200

    Finished image converter

---
+++ src/backend/util/image_converter.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import numpy as np
+import cv2
+
+
+def convert_string_to_pillow_image(image_str):
+    file_bytes = np.fromstring(image_str, np.uint8)
+    decoded_image = cv2.imdecode(file_bytes, cv2.IMREAD_UNCHANGED)
+    converted_image = cv2.cvtColor(decoded_image, cv2.COLOR_BGR2RGB)
+    pillow_image = Image.fromarray(converted_image)
+    return pillow_image
+
+
+def convert_string_to_cv2_image(image_str):
+    file_bytes = np.fromstring(image_str, np.uint8)
+    decoded_image = cv2.imdecode(file_bytes, cv2.IMREAD_UNCHANGED)
+    converted_image = cv2.cvtColor(decoded_image, cv2.COLOR_BGR2RGB)
+    return converted_image
