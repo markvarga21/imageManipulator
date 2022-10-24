6c0b7ac4847ac33dcc9cc68f2fe3d84e3d03f44a
Varga József Márk <vmark2145@gmail.com>
2022-10-24 09:54:01 +0200

    Added blur functionality

--- src/backend/service/retouch_service.py
+++ src/backend/service/retouch_service.py
@@ -1,4 +1,4 @@
-from PIL import ImageEnhance
+from PIL import ImageEnhance, ImageFilter
 
 from util.cleaner_service import delete_temp_files
 from util.temp_file_creator import return_temp_file
@@ -14,8 +14,14 @@ def get_retouch_image_file(pillow_img, retouch_type, new_value):
         enhancer = ImageEnhance.Color(pillow_img)
     elif retouch_type == 'sharpness':
         enhancer = ImageEnhance.Sharpness(pillow_img)
+    elif retouch_type == 'blur':
+        blured_image = pillow_img.filter(ImageFilter.GaussianBlur(new_value))
+        temp_file = return_temp_file('.jpeg')
+        blured_image.save(temp_file)
+        return temp_file
     else:
         return f'Type {retouch_type} not recognised!'
     temp_file = return_temp_file('.jpeg')
     enhancer.enhance(new_value).save(temp_file)
     return temp_file
+
