7f54abbb426ebba896e6a6419daa1be225ea40bb
Fradi575 <78791893+Fradi575@users.noreply.github.com>
2022-10-30 22:23:10 +0100

    Delete src directory

--- src/src/backend/dto/ValueRange.py
+++
@@ -1,6 +0,0 @@
-class ValueRange:
-    def __init__(self, frontend_min, frontend_max, pillow_min, pillow_max):
-        self.frontend_min = frontend_min
-        self.frontend_max = frontend_max
-        self.pillow_min = pillow_min
-        self.pillow_max = pillow_max
--- src/src/backend/util/temp_file_creator.py
+++
@@ -1,6 +0,0 @@
-import tempfile
-
-
-def return_temp_file(suffix: str) -> tempfile.TemporaryFile:
-    temp_file = tempfile.TemporaryFile(mode='r+', suffix=suffix, prefix='IMAGE_MANIPULATOR', delete=False)
-    return temp_file
