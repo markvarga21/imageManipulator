ae345105750e10b98ee1029d82f69aaf6c44840f
Varga József Márk <vmark2145@gmail.com>
2022-11-04 11:45:55 +0200

    Modified build.sh

--- src/backend/build.sh
+++ src/backend/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-sudo apt install tesseract-ocr-all -y
+apt install tesseract-ocr-all -y
 pip install --upgrade pip
 pip install -r requirements.txt
