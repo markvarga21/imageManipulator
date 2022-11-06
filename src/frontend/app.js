da10e1b26c3990127bedea4bb7a5bd85b6cd03df
Gabszter <cseres.gabor8@gmail.com>
2022-11-06 18:32:34 +0100

    Changing var to let

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,6 +1,6 @@
 // Update the contrast slider value
-var cont_slider = document.getElementById("contrast-slider");
-var cont_output = document.getElementById("contrast-value");
+let cont_slider = document.getElementById("contrast-slider");
+let cont_output = document.getElementById("contrast-value");
 cont_output.innerHTML = cont_slider.value;
 
 cont_slider.oninput = function() {
@@ -8,8 +8,8 @@ cont_slider.oninput = function() {
 }
 
 // Update the brightness slider value
-var bright_slider = document.getElementById("brightness-slider");
-var bright_output = document.getElementById("brightness-value");
+let bright_slider = document.getElementById("brightness-slider");
+let bright_output = document.getElementById("brightness-value");
 bright_output.innerHTML = bright_slider.value;
 
 bright_slider.oninput = function() {
@@ -17,8 +17,8 @@ bright_slider.oninput = function() {
 }
 
 // Update the sharpness slider value
-var sharp_slider = document.getElementById("sharpness-slider");
-var sharp_output = document.getElementById("sharpness-value");
+let sharp_slider = document.getElementById("sharpness-slider");
+let sharp_output = document.getElementById("sharpness-value");
 sharp_output.innerHTML = sharp_slider.value;
 
 sharp_slider.oninput = function() {
@@ -26,8 +26,8 @@ sharp_slider.oninput = function() {
 }
 
 // Update the color slider value
-var col_slider = document.getElementById("color-slider");
-var col_output = document.getElementById("color-value");
+let col_slider = document.getElementById("color-slider");
+let col_output = document.getElementById("color-value");
 col_output.innerHTML = col_slider.value;
 
 col_slider.oninput = function() {
@@ -35,8 +35,8 @@ col_slider.oninput = function() {
 }
 
 // Update the blur slider value
-var bl_slider = document.getElementById("blur-slider");
-var bl_output = document.getElementById("blur-value");
+let bl_slider = document.getElementById("blur-slider");
+let bl_output = document.getElementById("blur-value");
 bl_output.innerHTML = bl_slider.value;
 
 bl_slider.oninput = function() {
